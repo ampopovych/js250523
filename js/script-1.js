8a84a9385687be42c2be04e41261b4511ce565d8
Andriy Popovych <viktpopo@gmail.com>
2023-05-25 12:33:56 +0300

    25051233

---
+++ js/script-1.js
@@ -0,0 +1,6 @@
+const a='zminna a';
+console.log(a);
+const c='zminna c';
+console.log(c);
+const b='zminna b';
+console.log(b);
---
+++ js/script-3.js
@@ -0,0 +1,9 @@
+const age = 10;
+
+const totalPrice =200.34;
+
+const userName ='Andriy';
+const message = 'Vitajemo';
+const isOpen = true;
+const shouldConfirm = false;
+console.log('Cina',totalPrice)
